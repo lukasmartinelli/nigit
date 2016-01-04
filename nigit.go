9f7d731bb27ffd96d9f133906b20601e0e242a38
Lukas Martinelli <me@lukasmartinelli.ch>
2016-01-04 18:13:25 +0100

    Use codegangsta cli

--- nigit.go
+++ nigit.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,6 +10,8 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"github.com/codegangsta/cli"
 )
 
 func execProgram(program string, input string) string {
@@ -26,17 +29,49 @@ func execProgram(program string, input string) string {
 }
 
 func main() {
-	args := os.Args[1:]
-	program, _ := filepath.Abs(args[0])
-
-	serve := func(w http.ResponseWriter, r *http.Request) {
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-
-		stdout := execProgram(program, buf.String())
-		io.WriteString(w, stdout)
+	app := cli.NewApp()
+	app.Name = "nigit"
+	app.Version = "0.1-alpha"
+	app.Usage = "Expose any Program as HTTP API"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "port",
+			Value:  "5432",
+			Usage:  "HTTP port",
+			EnvVar: "PORT",
+		},
 	}
 
-	http.HandleFunc("/", serve)
-	http.ListenAndServe(":8000", nil)
+	app.Action = func(c *cli.Context) {
+		program := c.Args().First()
+		if program == "" {
+			fmt.Println("Please provide name of the script to run under nigit")
+			os.Exit(1)
+		}
+
+		program, err := filepath.Abs(program)
+		if err != nil {
+			fmt.Printf("Cannot get path of %s\n", program)
+			os.Exit(2)
+		}
+
+		programPath, err := exec.LookPath(program)
+		if err != nil {
+			fmt.Printf("Executable program %s not found\n", program)
+			os.Exit(3)
+		}
+
+		serve := func(w http.ResponseWriter, r *http.Request) {
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(r.Body)
+
+			stdout := execProgram(programPath, buf.String())
+			io.WriteString(w, stdout)
+		}
+
+		http.HandleFunc("/", serve)
+		http.ListenAndServe(":8000", nil)
+	}
+
+	app.Run(os.Args)
 }
